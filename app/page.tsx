d56cc479eac972629403a5554f7dbf593b0e491c
joellaprade <145079600+joellaprade@users.noreply.github.com>
2025-07-26 19:07:04 -0600

    s

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ const Page = () => {
     <div>
       <h1 className="mt-48">Welcome</h1>
       <div className="absolute-center flex flex-col gap-4 w-full px-8">
-        <NavBtn href="/signup" className="big main" text="Crear Cuenta" />
-        <NavBtn href="/login" className="big secondary" text="Ingresar" />
+        <NavBtn href="/signup" className="big main" text="Sign Up" />
+        <NavBtn href="/login" className="big secondary" text="Login" />
       </div>
     </div>
   );
