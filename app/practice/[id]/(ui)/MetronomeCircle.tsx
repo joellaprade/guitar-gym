d9852ff11e9866b05e4aedd8851a4ea0612ef14f
joellaprade <145079600+joellaprade@users.noreply.github.com>
2025-09-29 14:39:51 -0600

    difk

--- app/practice/[id]/(ui)/MetronomeCircle.tsx
+++ app/practice/[id]/(ui)/MetronomeCircle.tsx
@@ -36,11 +36,11 @@ const MetronomeCircle = () => {
       {isPlaying ? (
         <>
           {currentExercise.isTimeSelected ? (
-            <h1 className="text-dark-gray text-7xl font-bold">{formatTime(currentSecond)}</h1>
+            <h1 className="text-dark-gray text-8xl font-bold">{formatTime(currentSecond)}</h1>
           ) : (
             <div className="relative z-2 flex flex-col items-center justify-center">
-              <h1 className="text-dark-gray text-7xl font-bold">{currentExercise.bpm}</h1>
-              <h3 className="text-dark-gray absolute bottom-0 translate-y-[65%] font-bold">bpm</h3>
+              <h1 className="text-dark-gray text-8xl font-bold">{currentExercise.bpm}</h1>
+              <h3 className="text-dark-gray absolute bottom-0 translate-y-[65%] text-3xl font-bold">bpm</h3>
             </div>
           )}
         </>
